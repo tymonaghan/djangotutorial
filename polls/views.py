10213deab3a00cdafc3755d026a05db677f9654a
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-04-11 17:05:23 -0500

    refactor results view to use get_object_or_404

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
-from django.http import Http404
-
 
 # Create your views here.
 from django.http import HttpResponse
@@ -14,10 +12,9 @@ def index(request):
     # args: request object, template name, dictionary(optional)
 
 def detail(request, question_id):
-  try:
-    question = Question.objects.get(pk=question_id)
-  except Question.DoesNotExist:
-    raise Http404("Question does not exist. 404.")
+  # get_object_or_404 replaces a try/except block
+  # use get_list_or_404 if you want to return a list (use filter() instead of get()), which will still raise Http404 if the list is empty.
+  question = get_object_or_404(Question, pk=question_id)
   return render(request, "polls/detail.html", {'question':question})
   
 def results(request, question_id):
