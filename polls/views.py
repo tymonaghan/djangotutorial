559acc7075de6fc409988054d788675b8eed4ed7
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-04-12 17:01:47 -0500

    can now vote and view results

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from .models import Question, Choice
+from django.urls import reverse
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -18,11 +19,26 @@ def detail(request, question_id):
   return render(request, "polls/detail.html", {'question':question})
   
 def results(request, question_id):
-  response="You're looking at the results of question %s."
-  return HttpResponse(response % question_id)
-  # I'm not really sure why we're interpolating the variables differently in detail vs. results, i think just for demonstration purposes.
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {'question':question}) # so remember, it's request object, template name, optional dictionary. so polls/results.html is the template in this case.
 
 def vote(request, question_id):
-  return HttpResponse("You're voting on question %s." % question_id)
-  
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        # request.POST is a dictionary-like object that lets you access submitted data by key name. In this case, request.POST['choice'] returns the ID of the selected choice, as a string. request.POST values are always strings.
+        # request.POST['choice'] will raise KeyError if choice wasn’t provided in POST data. The above code checks for KeyError and redisplays the question form with an error message if choice isn’t given
+    except (KeyError, Choice.DoesNotExist):
+      # re-display the question voting form
+      return render(request, 'polls/detail.html',{
+        'question': question,
+        'error_message': "You must select a choice."
+      })
+    else: 
+      selected_choice.votes +=1
+      selected_choice.save()
+      #always return HttpResponseRedirect after dealing with POST data to avoid BACK button posting data 2x
+      #  HttpResponseRedirect takes a single argument: the URL to which the user will be redirected
+      return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+      # reverse() takes the name of the view that we want to pass control to (polls:results)
+      # so this would result in a path like /polls/:questionId/results/
